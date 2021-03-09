520700e3f80fc7886e4ae1c31d2ff7f642cc1d61
jnjohnny <19-39366-1@student.aiub.edu>
2021-03-09 19:12:19 +0600

    address change

--- lab_assignment/Address.cs
+++ lab_assignment/Address.cs
@@ -8,5 +8,62 @@ namespace lab_assignment
 {
     class Address
     {
+            private string roadNo;
+            private string houseNo;
+            private string city;
+            private string country;
+
+            public Address()
+            {
+                //this.houseNo = houseNo;
+                //this.roadNo = roadNo;
+                //this.city = city;
+                //this.country = country;
+
+            }
+
+
+            public string HouseNo
+            {
+                set { this.houseNo = value; }
+                get { return this.houseNo; }
+            }
+
+
+
+            public string RoadNo
+            {
+                set { this.roadNo = value; }
+                get { return this.roadNo; }
+            }
+
+
+
+            public string City
+            {
+                set { this.city = value; }
+                get { return this.city; }
+            }
+
+
+
+            public string Country
+            {
+                set { this.country = value; }
+                get { return this.country; }
+            }
+
+
+
+
+            public string GetAddress()
+            {
+                string ads = "Road No is =" + roadNo + "," + "House No is =" + houseNo + "," + "City :" + city + "," + "Country :" + country;
+                Console.WriteLine("Address:House No-{0},Road No-{1},City-{2},Country-{3}", this.houseNo, this.roadNo, this.city, this.country);
+                return ads;
+            }
+
+        }
+
     }
-}
+
