e50458b0b3b37107c4315ffa5fefac01a82c2c6e
jnjohnny <19-39366-1@student.aiub.edu>
2021-03-09 19:33:46 +0600

    Account Inheritance

--- lab_assignment/CheckingAccount.cs
+++ lab_assignment/CheckingAccount.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace lab_assignment
 {
-    class CheckingAccount
+    class CheckingAccount : Account
     {
+        public CheckingAccount(Address address) : base(address)
+        {
+
+        }
+
+        public override void Withdraw(double amount)
+        {
+            if (amount > 0 && amount <= balance)
+            {
+                Console.WriteLine("Previous Balance:	" + balance);
+                Console.WriteLine("Withdraw Amount:	" + amount);
+                balance -= amount;
+                Console.WriteLine("Current Balance:	" + balance);
+            }
+            else
+            {
+                Console.WriteLine("Can Not Withdraw");
+            }
+        }
+
+
     }
 }
--- lab_assignment/SavingAccount.cs
+++ lab_assignment/SavingAccount.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace lab_assignment
 {
-    class SavingAccount
+    class SavingAccount: Account
     {
+        public SavingAccount(Address address) : base(address)
+        {
+
+        }
+
+        public override void Withdraw(double amount)
+        {
+            if (amount > 0 && amount < balance)
+            {
+                Console.WriteLine("Previous Balance:	" + balance);
+                Console.WriteLine("Withdraw Amount:	" + amount);
+                balance -= amount;
+                Console.WriteLine("Current Balance:	" + balance);
+            }
+            else
+            {
+                Console.WriteLine("Can Not Withdraw");
+            }
+        }
     }
 }
